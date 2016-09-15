811dc65ebb6442cc8d2d165665197aada7768bc9
Marvin Sielenkemper <sielenk@gmx.de>
2016-09-15 08:42:21 +0200

    defined the numbers on top of sets, with less axioms, updated the proofs

--- ConwaySurreals.v
+++ ConwaySurreals.v
@@ -1,32 +1,31 @@
 Require Import Setoids.Setoid.
 
+Axiom U: Set.
 
-Axiom preNum: Set.
+Definition set := U -> Prop.
 
-Definition set := preNum -> Prop.
+Record preNum := mkPreNum {
+  L : set;
+  R : set
+}.
 
-Axiom L: preNum -> set.
-Axiom R: preNum -> set.
+Axiom embed: preNum -> U.
+Axiom embedInjective: forall n1 n2, embed n1 = embed n2 -> n1 = n2.
+
 
 Definition liftL(R: preNum -> preNum -> Prop)(l: set)(r: preNum): Prop :=
-  forall ul, l ul -> R ul r.
+  forall ul, l (embed ul) -> R ul r.
 
 Definition liftR(R: preNum -> preNum -> Prop)(l: preNum)(r: set): Prop :=
-  forall ur, r ur -> R l ur.
+  forall ur, r (embed ur) -> R l ur.
 
 Definition lift(R: preNum -> preNum -> Prop)(l: set)(r: set): Prop :=
-  forall ul ur, l ul -> r ur -> R ul ur.
-
-Axiom mkNum: set -> set -> preNum.
-
-Axiom mkNumLR: forall n,   n = mkNum (L n) (R n).
-Axiom mkNumL:  forall l r, l = L (mkNum l r).
-Axiom mkNumR:  forall l r, r = R (mkNum l r).
+  forall ul ur, l (embed ul) -> r (embed ur) -> R ul ur.
 
 Definition empty: set := fun _ => False.
-Definition zero_      := mkNum empty empty.
-Definition n_one_     := mkNum empty (eq zero_).
-Definition p_one_     := mkNum (eq zero_) empty.
+Definition zero_      := mkPreNum empty empty.
+Definition n_one_     := mkPreNum empty (eq (embed zero_)).
+Definition p_one_     := mkPreNum (eq (embed zero_)) empty.
 
 Axiom      le: preNum -> preNum -> Prop.
 Definition ge m n :=  le n m.
@@ -43,40 +42,23 @@ Definition num := sig isNumber.
 
 Lemma zeroIsNumber: isNumber zero_.
 Proof.
-  intros m n H1.
-  unfold zero_ in H1.
-  rewrite <- mkNumL in H1.
-  destruct H1.
+  intros m n [].
 Qed.
 
 Lemma nOneIsNumber: isNumber n_one_.
 Proof.
-  intros m n H1 H2.
-  unfold n_one_ in H1.
-  rewrite <- mkNumL in H1.
-  destruct H1.
+  intros m n [].
 Qed.
 
 Lemma pOneIsNumber: isNumber p_one_.
 Proof.
-  intros m n H1 H2.
-  unfold p_one_ in H2.
-  rewrite <- mkNumR in H2.
-  destruct H2.
+  intros m n H1 [].
 Qed.
 
 Lemma zeroLeZero: le zero_ zero_.
 Proof.
   rewrite leRec.
-  split.
-  unfold liftL, lt, zero_.
-  intros ul H1 H2.
-  rewrite <- mkNumL in H1.
-  destruct H1.
-  unfold liftR, lt, zero_.
-  intros ul H1 H2.
-  rewrite <- mkNumR in H1.
-  destruct H1.
+  split; intros _ [].
 Qed.
 
 Lemma nOneLtZero: lt n_one_ zero_.
@@ -84,10 +66,7 @@ Proof.
   intros H1.
   rewrite leRec in H1.
   destruct H1 as [H1 H2].
-  unfold liftR, zero_, lt, not in H2.
   apply (H2 zero_); try apply zeroLeZero.
-  unfold n_one_.
-  rewrite <- mkNumR.
   reflexivity.
 Qed.
 
@@ -96,10 +75,7 @@ Proof.
   intros H1.
   rewrite leRec in H1.
   destruct H1 as [H1 H2].
-  unfold liftL, zero_, lt, not in H1.
   apply (H1 zero_); try apply zeroLeZero.
-  unfold p_one_.
-  rewrite <- mkNumL.
   reflexivity.
 Qed.
 
@@ -107,15 +83,14 @@ Lemma nOneLenOne: le n_one_ n_one_.
 Proof.
   rewrite leRec.
   split.
-  unfold liftL, n_one_, lt, not.
-  rewrite <- mkNumL.
   intros _ [].
-  unfold liftR, n_one_, lt, not.
-  rewrite <- mkNumR.
-  intros _ [].
+  intros x H1 H2. simpl in H1.
+  apply embedInjective in H1.
+  destruct H1.
   apply nOneLtZero.
+  assumption.
 Qed.
 
-Definition zero: num := exist _ zero_ zeroIsNumber.
+Definition zero: num := exist _ zero_  zeroIsNumber.
 Definition nOne: num := exist _ n_one_ nOneIsNumber.
 Definition pOne: num := exist _ p_one_ pOneIsNumber.

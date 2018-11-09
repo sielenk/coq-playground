4b8b7dade572b6ee1b61c67ab6ab1ecdd3bacca4
Marvin Sielenkemper <sielenk@gmx.de>
2018-11-09 18:30:59 +0100

    added retaction, section, isomorphism, epi and mono morphisms

--- Category.v
+++ Category.v
@@ -44,6 +44,94 @@ Instance comp_eq_Proper{A: Cat}{X Y Z}: Proper (eq_h ==> eq_h ==> eq_h) (@comp A
   comp_eq A X Y Z.
 
 
+Polymorphic Record Ret{A: CatSig}{X Y: Ob A} := {
+  ret_hom :> Hom X Y;
+  ret_sec :  Hom Y X;
+  ret_prop:  eq_h (comp ret_hom ret_sec) (id Y)
+}.
+
+Arguments Ret {_} _ _.
+
+
+Polymorphic Record Sec{A: CatSig}{X Y: Ob A} := {
+  sec_hom :> Hom X Y;
+  sec_ret :  Hom Y X;
+  sec_prop:  eq_h (comp sec_ret sec_hom) (id X)
+}.
+
+Arguments Sec {_} _ _.
+
+
+Polymorphic Record Iso{A: CatSig}{X Y: Ob A} := {
+  iso_hom :> Hom X Y;
+  iso_inv :  Hom Y X;
+  iso_prop:  eq_h (comp iso_hom iso_inv) (id Y) /\
+             eq_h (comp iso_inv iso_hom) (id X)
+}.
+
+Arguments Iso {_} _ _.
+
+
+Polymorphic Record Mon{A: CatSig}{X Y: Ob A} := {
+  mon_hom :> Hom X Y;
+  mon_prop:  forall{W}(g1 g2: Hom W X), eq_h (comp mon_hom g1) (comp mon_hom g2) -> eq_h g1 g2
+}.
+
+Arguments Mon {_} _ _.
+
+
+Polymorphic Record Epi{A: CatSig}{X Y: Ob A} := {
+  epi_hom :> Hom X Y;
+  epi_prop:  forall{Z}(g1 g2: Hom Y Z), eq_h (comp g1 epi_hom) (comp g2 epi_hom) -> eq_h g1 g2
+}.
+
+Arguments Epi {_} _ _.
+
+
+Polymorphic Definition sec_mon{A: Cat}{X Y: Ob A}(f: Sec X Y): Mon X Y.
+  refine {| mon_hom := f |}.
+  intros.
+  transitivity (comp (id _) g2).
+  transitivity (comp (comp (sec_ret f) f) g2).
+  transitivity (comp (sec_ret f) (comp f g2)).
+  transitivity (comp (sec_ret f) (comp f g1)).
+  transitivity (comp (comp (sec_ret f) f) g1).
+  transitivity (comp (id _) g1).
+  symmetry. apply (ident_l A).
+  f_equiv.
+  symmetry. apply (sec_prop f).
+  symmetry. apply (assoc A).
+  f_equiv.
+  assumption.
+  apply (assoc A).
+  f_equiv.
+  apply (sec_prop f).
+  apply (ident_l A).
+Defined.
+
+
+Polymorphic Definition ret_epi{A: Cat}{X Y: Ob A}(f: Ret X Y): Epi X Y.
+  refine {| epi_hom := f |}.
+  intros.
+  transitivity (comp g1 (id _)).
+  symmetry. apply (ident_r A).
+  transitivity (comp g1 (comp f (ret_sec f))).
+  f_equiv.
+  symmetry. apply (ret_prop f).
+  transitivity (comp (comp g1 f) (ret_sec f)).
+  apply (assoc A).
+  transitivity (comp (comp g2 f) (ret_sec f)).
+  f_equiv.
+  assumption.
+  transitivity (comp g2 (comp f (ret_sec f))).
+  symmetry. apply (assoc A).
+  transitivity (comp g2 (id _)).
+  f_equiv.
+  apply (ret_prop f).
+  apply (ident_r A).
+Defined.
+
+
 Polymorphic Definition isomorphic{A: CatSig}: relation A :=
   fun X Y => exists f g, eq_h (comp f g) (id Y) /\ eq_h (comp g f) (id X).
 
